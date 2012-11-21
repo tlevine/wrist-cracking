d0b2ca6adefbd647994a74a9add23c4aa25255ba
Thomas Levine <occurrence@thomaslevine.com>
2012-11-21 13:05:10 -0500

    citation

--- collect/video.sh
+++ collect/video.sh
@@ -7,4 +7,5 @@ fi
 
 outfile="$1"
 
+# http://gstreamer-devel.966125.n4.nabble.com/how-to-create-a-pipeline-to-record-video-from-camera-td3134399i20.html
 gst-launch-0.10 -e v4l2src ! 'video/x-raw-yuv,width=320,height=240, framerate=30/1' ! ffmpegcolorspace ! x264enc ! mp4mux ! filesink location="$outfile"
