c6c3b04ff284e56ca8b49f1bac9f7b7e8b80434f
Thomas Levine <occurrence@thomaslevine.com>
2012-11-21 12:49:33 -0500

    add backend tests

---
+++ annotate/test/test_backend.py
@@ -0,0 +1,10 @@
+class TestAnnotator:
+    def test_index_json_lists_trials(self):
+        'The annotator endpoint, /annotations/ is a json file with an index listing.'
+        pass
+
+    def test_get_annotation(self):
+        'GET /annotations/001.json should return the database row.'
+
+    def test_post_annotation(self):
+        'POSTing the annotation should modify it in the database.'
