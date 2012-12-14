71487a2a6c10b406ac35005ed1e541a6b86d5905
Thomas Levine <occurrence@thomaslevine.com>
2012-12-14 23:05:19 +0000

    Skeleton

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,11 +1,26 @@
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("date");
+  Serial.println("date,                           extensor_emg, flexor_emg, sync_button_pressed");
+}
+
+int sync_button_pressed(){
+  // Check whether the sync button is pressed.
+  // (The button press is recorded on video to ease syncing.)
+  return 0;
+}
+
+int flexor_emg() {
+  // Reading on the flexor emg pin
+  return 123;
+}
+int extensor_emg() {
+  // Reading on the flexor emg pin
+  return 123;
 }
 
 void loop()
 {
-  Serial.println("aoeu");
+  Serial.println("2012-11-21 11:20:43.007000000,           123,        123, 0");
   delay(1000);
 }
