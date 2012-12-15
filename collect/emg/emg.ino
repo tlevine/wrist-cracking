5e5edc34576a595e11259c8eaead4687e654bb92
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 00:03:49 +0000

    More sekeleton

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,7 +1,5 @@
-void setup()
-{
-  Serial.begin(9600);
-  Serial.println("date,                           extensor_emg, flexor_emg, sync_button_pressed");
+char* iso_date() {
+  return "2012-11-21 11:20:43.007000000";
 }
 
 int sync_button_pressed(){
@@ -19,8 +17,31 @@ int extensor_emg() {
   return 123;
 }
 
+int flexor_emg_pin = 5;
+int extensor_emg_pin = 6;
+
+char* i = "2012-11-21 11:20:43.007000000";
+int f;
+int e;
+int b;
+
+void setup()
+{
+  pinMode(flexor_emg_pin, INPUT);
+  pinMode(extensor_emg_pin, INPUT);
+
+  Serial.begin(9600);
+  Serial.println("date,                           extensor_emg, flexor_emg, sync_button_pressed");
+}
+
 void loop()
 {
   Serial.println("2012-11-21 11:20:43.007000000,           123,        123, 0");
+  i = iso_date();
+  f = flexor_emg();
+  e = extensor_emg();
+  b = sync_button_pressed();
+  // How do I do printf?
+  Serial.println("%s,%014d,%014d,%d", i,f,e,b);
   delay(1000);
 }
