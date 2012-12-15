2fdd022891437a43b124f23aa775a7c3e54f3828
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 15:53:44 +0000

    Simplify

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,40 +1,33 @@
+int flexor_emg() {
+  // Reading on the flexor emg pin
+  return analogRead(1);
+}
+int extensor_emg() {
+  // Reading on the flexor emg pin
+  return analogRead(2);
+}
+
 int sync_button_pressed(){
   // Check whether the sync button is pressed.
   // (The button press is recorded on video to ease syncing.)
   return 0;
 }
 
-int flexor_emg() {
-  // Reading on the flexor emg pin
-  return 123;
-}
-int extensor_emg() {
-  // Reading on the flexor emg pin
-  return 123;
-}
-
-int flexor_emg_pin = 5;
-int extensor_emg_pin = 6;
-
-int f;
-int e;
-int b;
-int t;
-
 void setup()
 {
-  pinMode(flexor_emg_pin, INPUT);
-  pinMode(extensor_emg_pin, INPUT);
-
   Serial.begin(9600);
-  Serial.println("date,                           extensor_emg, flexor_emg, sync_button_pressed");
+  Serial.println("millis(),extensor_emg, flexor_emg, sync_button_pressed");
 }
 
 void loop()
 {
-  f = flexor_emg();
-  e = extensor_emg();
-  b = sync_button_pressed();
+  Serial.print(millis());
+  Serial.print(',');
+  Serial.print(flexor_emg());
+  Serial.print(',');
+  Serial.print(extensor_emg());
+  Serial.print(',');
+  Serial.print(sync_button_pressed());
   Serial.println(); 
   delay(1000);
 }
