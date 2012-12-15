b2c710cf4a5b6eb19b7977d5f049d0ed08a9e423
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 14:22:42 -0500

    skeleton for plots

---
+++ annotate/plot.r
@@ -0,0 +1,7 @@
+#!/usr/bin/Rscript --vanilla
+# Convert an emg or audio csv into a png plot
+library(ggplot2)
+
+png('foo.png')
+
+dev.off()
---
+++ annotate/plot.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/Rscript --vanilla
+# Convert an emg or audio csv into a png plot
+
+# Plot
+./plot.r
+
+# Crop
+convert blah blah
