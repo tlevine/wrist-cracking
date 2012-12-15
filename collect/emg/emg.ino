63f743caab09268ac36dbbb0a24477d8b72f2952
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 15:46:14 +0000

    Remove time junk
    Setting the time is annoying, so let's just use millis()

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,10 +1,3 @@
-#include <Time.h>
-#include <TimeAlarms.h>
-
-char* iso_date() {
-  return "2012-11-21 11:20:43.007000000";
-}
-
 int sync_button_pressed(){
   // Check whether the sync button is pressed.
   // (The button press is recorded on video to ease syncing.)
@@ -23,34 +16,10 @@ int extensor_emg() {
 int flexor_emg_pin = 5;
 int extensor_emg_pin = 6;
 
-char* i = "2012-11-21 11:20:43.007000000";
 int f;
 int e;
 int b;
 int t;
-
-void printPaddedDigits(int digits)
-{
-  if(digits < 10)
-    Serial.print('0');
-  Serial.print(digits);
-}
-
-void printIso8601() {
-  // Print the datetime in ISO 8601 format.
-  t = now();
-  Serial.print(year(t));
-  Serial.print("-");
-  Serial.print(month(t));
-  Serial.print("-");
-  Serial.print(day(t));
-  Serial.print("T");
-  Serial.print(hour(t));
-  Serial.print(":");
-  printPaddedDigits(minute());
-  Serial.print(":");
-  printPaddedDigits(second());
-}
 
 void setup()
 {
@@ -63,21 +32,9 @@ void setup()
 
 void loop()
 {
-  //Serial.println("2012-11-21 11:20:43.007000000,           123,        123, 0");
-  i = iso_date();
   f = flexor_emg();
   e = extensor_emg();
   b = sync_button_pressed();
-  // How do I do printf?
-  //Serial.println("%s,%014d,%014d,%d", i,f,e,b);
-  printIso8601();
-//  Serial.print(i);
-//  Serial.print(',');
-//  Serial.print(f, DEC);
-//  Serial.print(',');
-//  Serial.print(e, DEC);
-//  Serial.print(',');
-//  Serial.print(b, DEC);
   Serial.println(); 
   delay(1000);
 }
