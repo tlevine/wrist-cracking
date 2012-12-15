49241d82dc0165f738afb3ba609e90e67327b4e9
Thomas Levine <occurrence@thomaslevine.com>
2012-12-15 16:12:44 +0000

    Light LED and tidy stuff

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,3 +1,6 @@
+int syncButtonPin = 6;
+int ledPin = 13;
+
 int flexor_emg() {
   // Reading on the flexor emg pin
   return analogRead(1);
@@ -10,16 +13,25 @@ int extensor_emg() {
 int sync_button_pressed(){
   // Check whether the sync button is pressed.
   // (The button press is recorded on video to ease syncing.)
-  return 0;
+  if (HIGH == digitalRead(syncButtonPin)){
+    digitalWrite(ledPin, HIGH);
+    return 0;
+  } else {
+    digitalWrite(ledPin, LOW);
+    return 1;
+  }
 }
 
 void setup()
 {
+  pinMode(syncButtonPin, INPUT);
+  pinMode(ledPin, INPUT);
+  digitalWrite(ledPin, LOW);
   Serial.begin(9600);
-  Serial.println("millis(),extensor_emg, flexor_emg, sync_button_pressed");
+  Serial.println("millis(),extensor_emg,flexor_emg,sync_button_pressed");
 }
 
-void loop()
+void loop()q
 {
   Serial.print(millis());
   Serial.print(',');
@@ -29,5 +41,5 @@ void loop()
   Serial.print(',');
   Serial.print(sync_button_pressed());
   Serial.println(); 
-  delay(1000);
+  delay(99);
 }
