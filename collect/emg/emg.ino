acab5754f36d21720e0be5dcf99192b0c5fce43a
Thomas Levine <occurrence@thomaslevine.com>
2012-12-16 22:47:40 +0000

    Adapt for Boarduino

--- collect/emg/emg.ino
+++ collect/emg/emg.ino
@@ -1,37 +1,41 @@
-int syncButtonPin = 6;
+int speakerPin = 11;
+int syncButtonPin = 8;
 int ledPin = 13;
+int button_state;
 
 int flexor_emg() {
   // Reading on the flexor emg pin
-  return analogRead(1);
+  return analogRead(4);
 }
 int extensor_emg() {
   // Reading on the flexor emg pin
-  return analogRead(2);
+  return analogRead(5);
 }
 
 int sync_button_pressed(){
   // Check whether the sync button is pressed.
   // (The button press is recorded on video to ease syncing.)
-  if (HIGH == digitalRead(syncButtonPin)){
-    digitalWrite(ledPin, HIGH);
-    return 0;
-  } else {
-    digitalWrite(ledPin, LOW);
-    return 1;
-  }
+  button_state = digitalRead(syncButtonPin);
+
+  // Yell if and only if pressed.
+  digitalWrite(ledPin, button_state);
+  digitalWrite(speakerPin, button_state);
+  
+  // Return 0 for false instead of for true.
+  return (button_state == HIGH);
 }
 
 void setup()
 {
   pinMode(syncButtonPin, INPUT);
   pinMode(ledPin, INPUT);
+  pinMode(speakerPin, OUTPUT);
   digitalWrite(ledPin, LOW);
   Serial.begin(9600);
   Serial.println("millis(),extensor_emg,flexor_emg,sync_button_pressed");
 }
 
-void loop()q
+void loop()
 {
   Serial.print(millis());
   Serial.print(',');
