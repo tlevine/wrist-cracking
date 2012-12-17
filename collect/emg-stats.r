b9399c4e0c8ada612227bc26844f5877d7cce1f7
Thomas Levine <occurrence@thomaslevine.com>
2012-12-17 14:39:44 -0500

    add a plot

--- collect/emg-stats.r
+++ collect/emg-stats.r
@@ -2,12 +2,18 @@
 
 filename <- commandArgs()[1]
 emg <- read.csv(filename)
-emg$sync_button_previous_state <- emg$sync_button_pressed[c(0, 1:(nrow(emg) - 1))]
 
-emg.switch <- subset(emg, sync_button_pressed != sync_button_previous_state)
+emg.sync <- function(emg) {
+  emg$sync_button_previous_state <- emg$sync_button_pressed[c(0, 1:(nrow(emg) - 1))]
+  emg.switch <- subset(emg, sync_button_pressed != sync_button_previous_state)
 
-# Proportional sync points (0 to 1)
-sync.points <- emg.switch[c(1, nrow(emg.switch)), 'millis()'] / max(emg$millis)
+# oportional sync points (0 to 1)
+  sync.points <- emg.switch[c(1, nrow(emg.switch)), 'millis'] / max(emg$millis)
 
-cat(sync.points[1])
-cat(sync.points[2])
+  cat(sync.points[1])
+  cat(sync.points[2])
+}
+
+emg.plot <- function(emg) {
+  ggplot(emg) + aes(x = millis, y = emg1) + geom_line()
+}
